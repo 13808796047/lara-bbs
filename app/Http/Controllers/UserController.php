d36186432dfdafaa81fd3bb5180682cf8715e716
Summer <csy13808796047@gmail.com>
2020-08-07 15:36:50 +0800

    上传头像

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 
@@ -15,9 +16,16 @@ class UserController extends Controller
     {
         return view('users.edit', compact('user'));
     }
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('user.show', $user->id)->with('success', '个人资料更新成功!');
     }
 }
