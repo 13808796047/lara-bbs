6252921ce200adb0487156336e2a8af4c04c6ffc
Summer <csy13808796047@gmail.com>
2020-08-08 14:00:58 +0800

    用户可以编辑话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -2,14 +2,14 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use App\Models\Topic;
+use App\Models\User;
 
 class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
+        return $topic->user_id == $user->id;
         return true;
     }
 
