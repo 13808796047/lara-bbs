da37621e1464b7a43463d70f438e9fffa2bf5507
Summer <csy13808796047@gmail.com>
2020-08-11 14:51:49 +0800

    管理后台权限

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,4 +8,13 @@ class WelcomeController extends Controller
     {
         return view('welcome');
     }
+    public function permissionDenied()
+    {
+        // 如果当前用户有权限访问后台,直接跳转访问
+        if (\config('administrator.permission')()) {
+            return \redirect(url(\config('administrator.uri')), 302);
+        }
+        // 否则使用视图
+        return view('permission_denied');
+    }
 }
