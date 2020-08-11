47535524691d35d18937e3a772c675f64b7ba9c7
Summer <csy13808796047@gmail.com>
2020-08-11 11:26:24 +0800

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -4,9 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Reply;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -20,7 +22,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     public function toDatabase($notifiable)
@@ -38,5 +40,11 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link('#reply' . $this->reply->id);
+        return (new MailMessage)->line('您的话题有新回复!')
+            ->action('查看回复', $url);
     }
 }
