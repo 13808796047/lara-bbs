d1a71ef8bcc443d39f8da31ff2d62a200fb6cf19
Summer <csy13808796047@gmail.com>
2020-08-20 13:23:27 +0800

    推荐资源列表

--- routes/api.php
+++ routes/api.php
@@ -76,6 +76,9 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
             // 某个用户的回复列表
             Route::get('users/{user}/replies', 'RepliesController@userIndex')
                 ->name('users.replies.index');
+            // 资源推荐
+            Route::get('links', 'LinksController@index')
+                ->name('links.index');
             // 通知列表
             Route::get('notifications', 'NotificationsController@index')
                 ->name('notifications.index');
